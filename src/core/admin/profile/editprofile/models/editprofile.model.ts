b673d05f438d9efdccea6b6544b9bc47dd0d4a27
Jaydev Thomke <jaydev.thomke@firstcry.com>
2019-06-16 14:41:06 +0530

    initial commit

---
+++ src/core/admin/profile/editprofile/models/editprofile.model.ts
@@ -0,0 +1,33 @@
+/*
+* spurtcommerce
+* version 2.2
+* http://www.spurtcommerce.com
+*
+* Copyright (c) 2019 piccosoft ltd
+* Author piccosoft ltd <support@piccosoft.com>
+* Licensed under the MIT license.
+*/
+export class EditprofileForm {
+
+
+    public username: string;
+    public firstName: string;
+    public lastName: string;
+    public phoneNumber: string;
+    public email: string;
+    public address: string;
+    public avatar: string;
+
+
+    constructor(editprofileForm: any) {
+        this.username = editprofileForm.username || '';
+        this.firstName = editprofileForm.firstName || '';
+        this.lastName = editprofileForm.lastName || '';
+        this.phoneNumber = editprofileForm.phoneNumber || '';
+        this.email = editprofileForm.email || '';
+        this.address = editprofileForm.address || '';
+        if (editprofileForm.avatar) {
+            this.avatar = editprofileForm.avatar || '';
+        }
+    }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://pune-local-store.herokuapp.com/api',
+  imageUrl: 'https://pune-local-store.herokuapp.com/api/media/image-resize',
+  productUrl: 'https://pune-local-store-admin.herokuapp.com/#/'
+
+};
